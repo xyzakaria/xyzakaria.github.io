033fb954e51615c19a59c796d0f375b614661431
Ayman <ayman.elalass.etu@univ-lille.fr>
2024-09-07 00:07:22 +0200

    "fun fact"

--- script.js
+++ script.js
@@ -6,12 +6,23 @@ function calculateDaysUntil(targetDate) {
     return daysDifference;
 }
 
-function updateCountdown() {
-    const daysUntil = calculateDaysUntil('October 26, 2024');
-    const countdownElement = document.getElementById('countdown');
-    countdownElement.innerText = `J-${daysUntil}`;
+function calculateTimeUntil(targetDate) {
+    const currentDate = new Date();
+    const target = new Date(targetDate);
+    const timeDifference = target - currentDate;
+
+    const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hoursDifference = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+    return { days: daysDifference, hours: hoursDifference };
 }
 
-// Update the countdown immediately and then every day
+function updateCountdown() {
+    const timeUntil = calculateTimeUntil('October 26, 2024');
+    const countdownElement = document.getElementById('countdown');
+    countdownElement.innerText = `J-${timeUntil.days} et ${timeUntil.hours} heure(s)`;
+}
+
+// Update the countdown immediately and then every hour
 updateCountdown();
-setInterval(updateCountdown, 1000 * 60 * 60 * 24);
+setInterval(updateCountdown, 1000 * 60 * 60);
